0dd9f3d510cd6b28e0fe41aa9c542bc111c000e1
Hussein El Ghoul <husseinelghoul@yahoo.com>
2020-03-15 00:26:45 +0200

    :construction:

---
+++ RelationalNumber.py
@@ -0,0 +1,43 @@
+#Helper function to matain rational numbers in reduced form [BR-3]
+def computeGCD(x, y): 
+   while(y): 
+       x, y = y, x % y 
+   return x 
+
+class Rational:
+    def __init__(self,numerator=0,denomirator=1):
+        if denomirator==0:
+            raise ZeroDivisionError("Illegal Argument Exception")
+        factor = computeGCD(numerator,denomirator)
+        self.numerator = numerator//factor
+        self.denomirator = denomirator//factor
+    def __str__(self):
+        if (self.denomirator==1):
+            return str(self.numerator)
+        else:
+            return str(self.numerator) + "/" + str(self.denomirator)
+    def getDenominator(self):
+        return self.denomirator
+    def getNumerator(self):
+        return self.numerator
+    def __add__(self, other):
+        return Rational(self.numerator*other.denomirator + other.numerator*self.denomirator, self.denomirator * other.denomirator)
+    def __sub__(self,other):
+        return Rational(self.numerator*other.denomirator - other.numerator*self.denomirator, self.denomirator * other.denomirator)
+    def __mul__(self,other):
+        return Rational(self.numerator * other.numerator, self.denomirator * other.denomirator)
+    #dividing a fraction by another is the same as multiplying it by it's inverse
+    def __div__(self,other):
+        #y = inverse other
+        y = Rational(other.denomirator,other.numerator)
+        return self*y
+    
+#Testing
+x = Rational(3,-5)
+y = Rational(3,6)
+print(x)#Should print -3/5
+print(y)#Should print 3/6
+print(x+y)#Should print -1/10
+print(x-y)#Should print -11/10
+print(x*y)#Should print -3/10
+print(x/y)#Should print -6/5
