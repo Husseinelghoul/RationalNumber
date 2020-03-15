cb90946005a196597216ff3507d0f38b12300ddf
Ali ElZein <alielzein753@gmail.com>
2020-03-15 22:17:35 +0200

    some adjustments by the boss

--- RationalNumber.py
+++ RationalNumber.py
@@ -1,47 +1,66 @@
 #Helper function to matain rational numbers in reduced form [BR-3]
-def computeGCD(x, y): 
-   while(y): 
-       x, y = y, x % y 
-   return x 
+
 
 class Rational:
     def __init__(self,numerator=0,denomirator=1):
+        def computeGCD(x, y): 
+            while(y): 
+               x, y = y, x % y 
+            return x 
+
         if denomirator==0:
             raise ZeroDivisionError("IllegalArgumentException")
         factor = computeGCD(numerator,denomirator)
         self.numerator = numerator//factor
         self.denomirator = denomirator//factor
+
     def __str__(self):
         if (self.denomirator==1):
             return str(self.numerator)
         else:
             return str(self.numerator) + "/" + str(self.denomirator)
+
     def getDenominator(self):
         return self.denomirator
+
     def getNumerator(self):
         return self.numerator
+
     def __add__(self, other):
-        return Rational(self.numerator*other.denomirator + other.numerator*self.denomirator, self.denomirator * other.denomirator)
+        return Rational(
+            self.numerator*other.denomirator + other.numerator*self.denomirator, 
+            self.denomirator * other.denomirator
+        )
+
     def __sub__(self,other):
-        return Rational(self.numerator*other.denomirator - other.numerator*self.denomirator, self.denomirator * other.denomirator)
+        return Rational(
+            self.numerator*other.denomirator - other.numerator*self.denomirator, 
+            self.denomirator * other.denomirator
+        )
+
     def __mul__(self,other):
-        return Rational(self.numerator * other.numerator, self.denomirator * other.denomirator)
-    #dividing a fraction by another is the same as multiplying it by it's inverse
-    def __div__(self,other):
-        #y = inverse other
-        y = Rational(other.denomirator,other.numerator)
-        return self*y
+        return Rational(
+            self.numerator * other.numerator, 
+            self.denomirator * other.denomirator
+        )
+
+    def __truediv__(self,other):
+        return self*Rational(other.denomirator,other.numerator)
+
     def __eq__(self, other):
         return (self.numerator == other.numerator and self.denomirator == other.denomirator)
+
     def __ne__(self,other):
         return  not self==other
-    
-#Testing
-# x = Rational(3,-5)
-# y = Rational(3,6)
-# print(x)#Should print -3/5
-# print(y)#Should print 3/6
-# print(x+y)#Should print -1/10
-# print(x-y)#Should print -11/10
-# print(x*y)#Should print -3/10
-# print(x/y)#Should print -6/5
+
+# Testing
+if __name__ == "__main__":
+    # x = Rational(3,-5)
+    # y = Rational(3,6)
+    # print(x)#Should print -3/5
+    # print(y)#Should print 3/6
+    # print(x+y)#Should print -1/10
+    # print(x-y)#Should print -11/10
+    # print(x*y)#Should print -3/10
+    # print(x/y)#Should print -6/5
+    x = Rational(5, 0)
--- test_Rational.py
+++ test_Rational.py
@@ -12,46 +12,61 @@ class TestRationalMethods(unittest.TestCase):
         self.assertEqual(bool2,True,"Error in equal")
         self.assertEqual(bool3,False,"Error in equal")
         self.assertEqual(bool4,False,"Error in equal")
+
     #Testing Addition
     def test_add_pos(self):
         result = Rational(2,5)+Rational(2,5)
-        self.assertEqual(result,Rational(4,5),"Error in Adding Positive Fracrtionss")
+        self.assertEqual(result,Rational(4,5),"Error in Adding Positive Fractions")
+
     def test_add_neg(self):
         result = Rational(4,5)-Rational(3,5)
-        self.assertEqual(result,Rational(1,5),"Error in Adding Negative Fracrtions")
+        self.assertEqual(result,Rational(1,5),"Error in Adding Negative Fractions")
+
     #Testing Substraction
     def test_sub_pos(self):
         result = Rational(4,5)-Rational(2,5)
-        self.assertEqual(result,Rational(2,5),"Error in Substracting Positive Fracrtions")
+        self.assertEqual(result,Rational(2,5),"Error in Substracting Positive Fractions")
+
     def test_sub_neg(self):
         result = Rational(2,5)-Rational(-2,5)
-        self.assertEqual(result,Rational(4,5),"Error in Substracting Negative Fracrtions")
+        self.assertEqual(result,Rational(4,5),"Error in Substracting Negative Fractions")
+
     #Testing Multiplication
     def test_mult_pos(self):
         result = Rational(2,5)*Rational(2,5)
-        self.assertEqual(result,Rational(4,25),"Error multiplying  Positive fractions")
+        self.assertEqual(result,Rational(4,25),"Error multiplying Positive fractions")
+
     def test_mult_neg(self):
         result = Rational(2,5)*Rational(-2,5)
-        self.assertEqual(result,Rational(-4,25),"Error in multiplying  Negative fractions")
+        self.assertEqual(result,Rational(-4,25),"Error in multiplying Negative fractions")
+
     #Testing Division
     def test_div_pos(self):
         result = Rational(2,3)/Rational(5,7)
         self.assertEqual(result,Rational(14,15),"Error in dividing Positive fractions")
+
     def test_div_neg(self):
         result = Rational(2,3)/Rational(-5,7)
         self.assertEqual(result,Rational(-14,15),"Error in dividing Negative fractions")
+
 class TestRationalBRs(unittest.TestCase):
+    # def test_BR1(self):
+    #      x = Rational(5,0)
+    #      self.assertRaises(ZeroDivisionError,0,"IllegalArgumentException")
+
+    # def test_BR2(self):
+    #      x = Rational(5,0)
+    #      self.assertNotEqual(x.denomirator,0,"IllegalArgumentException")
+
     def test_BR3(self):
         x = Rational(3,6);
         y = Rational(1,2)
         self.assertEqual(x,y,"Error in B3")
+
     def test_BR4(self):
         x = Rational(-3,4)
         y = Rational(3,-4)
         self.assertEqual(x,y,"Error in BR4")
-    # def test_BR1(self):
-    #      x = Rational(5,0)
-    #      self.assertNotEqual(x.denomirator,0,"IllegalArgumentException")
         
 
 if __name__ == '__main__':
